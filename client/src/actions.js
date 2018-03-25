3f7bd5d6cd592cf0ae99ed04456e33abcc8f3751
Jinren Ping <jinren.ping@outlook.com>
2018-03-25 00:55:54 -0400

    Add login and materials actions

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,5 +1,10 @@
-export const REGISTRATION_REQUEST = 'REGISTRATION_REQUEST' 
+export const REGISTRATION_REQUEST = 'REGISTRATION_REQUEST'
 export const REGISTRATION_SUCCESS = 'REGISTRATION_SUCCESS'
+export const LOGIN_REQUEST = 'LOGON_REQUEST'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const GET_MATERIALS_REQUEST = 'GET_MATERIALS_REQUEST'
+export const GET_MATERIALS_SUCCESS = 'GET_MATERIALS_SUCCESS'
+export const REFRESH_MATERIALS = 'REFRESH_MATERIALS'
 
 function registrationRequest() {
   return {
@@ -15,6 +20,66 @@ function registrationSuccess(json) {
   }
 }
 
+function loginRequest(json) {
+  return {
+    type: LOGIN_REQUEST,
+    userId: json.userId
+  }
+}
+
+function loginSuccess() {
+  return {
+    type: LOGIN_SUCCESS
+  }
+}
+
+function getMaterialsRequest() {
+  return {
+    type: GET_MATERIALS_REQUEST
+  }
+}
+
+function getMaterialsSuccess(json) {
+  return {
+    type: GET_MATERIALS_SUCCESS,
+    materials: json
+  }
+}
+
+function getMaterials(userId) {
+  return dispatch => {
+    dispatch(getMaterialsRequest())
+    return fetch(`/users/${userId}/requests`)
+      .then(res => res.json())
+      .then(json => dispatch(getMaterialsSuccess(json)))
+  }
+}
+
+function shouldGetMaterials(state) {
+  const materials = state.materials
+  if (!materials) {
+    return true
+  } else if (materials.isFetching) {
+    return false
+  } else {
+    return materials.isOutdated
+  }
+}
+
+export function refreshMaterials(userId) {
+  return {
+    type: refreshMaterials    
+  }
+}
+
+export function getMaterialsIfNecessary(userId) {
+  return (dispatch, getState) => {
+    if (shouldGetMaterials(getState())) {
+      return dispatch(getMaterials(userId))
+    }
+  }
+}
+
 export function registerUser(userInfo) {
   console.log(userInfo)
   return dispatch => {
@@ -26,10 +91,21 @@ export function registerUser(userInfo) {
         'Content-Type': 'application/json'
       })
     }).then(res => res.json())
-    .then(json => dispatch(registrationSuccess(json)))
+      .then(json => dispatch(registrationSuccess(json)))
   }
 }
 
 export function loginUser(userInfo) {
-  return {}
+  console.log(userInfo)
+  return dispatch => {
+    dispatch(loginRequest())
+    return fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify(userInfo),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }).then(res => res.json())
+      .then(json => dispatch(loginSuccess(json)))
+  }
 }
